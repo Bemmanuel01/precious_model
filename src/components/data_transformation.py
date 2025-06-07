749002e9ebe903c4ad0fc7be2badf411d283b5d4
Bemmanuel01 <benjaminemmanuel2017@gmail.com>
2025-06-07 03:33:10 +0100

    prediction

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -41,7 +41,7 @@ class DataTransformation:
             cat_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy='most_frequent')),
-                    ("one_hot_encoder", OneHotEncoder()),
+                    ("one_hot_encoder", OneHotEncoder(handle_unknown='ignore')),
                     ("scaler", StandardScaler(with_mean=False))
                 ]
             )
@@ -68,7 +68,7 @@ class DataTransformation:
             preprocessing_obj = self.get_data_transformer_object()
             
             target_column_name = "math_score"
-            numerical_columns = ["writing", "reading"]
+            numerical_columns = ["writing_score", "reading_score"]
             
             input_feature_train_df = train_df.drop(columns = [target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
